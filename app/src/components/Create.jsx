67f2184b84915cd01461c319c8d07a5659957dd7
Nico Desktop <diaznicox96@gmail.com>
2023-06-29 21:08:08 -0300

    Update

---
+++ app/src/components/Create.jsx
@@ -0,0 +1,100 @@
+import { Form, Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { db } from '../firebaseConfig/firebase';
+import { collection, addDoc } from 'firebase/firestore';
+import Swal from 'sweetalert2';
+import { useState } from 'react';
+
+export const Create = () => {
+    const [nombre, setNombre] = useState("");
+    const [marca, setMarca] = useState("");
+    const [modelo, setModelo] = useState("");
+    const [estado, setEstado] = useState("");
+    const [cantidad, setCantidad] = useState(1);
+    const [antiguedad, setAntiguedad] = useState(0);
+
+    const equipmentCollection = collection(db, "medicalSupplies");
+
+    const navigate = useNavigate();
+
+    const createEquipment = async () => {
+        await addDoc(equipmentCollection, {
+            nombre: nombre,
+            marca: marca,
+            modelo: modelo,
+            estado: estado,
+            cantidad: Number(cantidad),
+            antiguedad: Number(antiguedad)
+        });
+    }
+
+    const confirmCreate = (e) => {
+        e.preventDefault();
+        Swal.fire({
+            title: '¿Confirma crear el equipo médico?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Crear',
+            cancelButtonText: 'Cancelar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                createEquipment(e)
+                .then(() => {
+                    Swal.fire(
+                        '¡Creado!',
+                        `Se ha creado un equipo médico exitosamente.`,
+                        'success'
+                    ).then(() => {navigate('/')});
+                })
+                .catch((error) => {
+                    Swal.fire(
+                        'Ha ocurrido un error.',
+                        `Razón: ${error}`,
+                        'error'
+                    );
+                })         
+            }
+          })
+    }
+    return (
+        <section className="d-flex justify-content-center mt-4">
+            <article className='d-flex flex-column align-items-center border border-2 rounded px-5 pe-5 pb-3' id='contenedorForm'>
+                <h2 className='mt-4 mb-3 fs-4'>Registro de un Equipo Médico</h2>
+                <Form onSubmit={confirmCreate}>
+                    <Form.Group className="mb-3 d-flex align-items-center gap-3" controlId="formNombre" value={nombre} onChange={(e) => setNombre(e.target.value)}>
+                        <Form.Label>Nombre: </Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3 d-flex align-items-center gap-3" controlId="formMarca" value={marca} onChange={(e) => setMarca(e.target.value)}>
+                        <Form.Label>Marca: </Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3 d-flex align-items-center gap-3" controlId="formModelo" value={modelo} onChange={(e) => setModelo(e.target.value)}>
+                        <Form.Label>Modelo: </Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3 d-flex align-items-center gap-3" controlId="formEstado" value={estado} onChange={(e) => setEstado(e.target.value)}>
+                        <Form.Label>Estado: </Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3 d-flex align-items-center gap-3" controlId="formCantidad" value={cantidad} onChange={(e) => setCantidad(e.target.value)}>
+                        <Form.Label className='text-center'>Cantidad: </Form.Label>
+                        <Form.Control type="number" min="1" max="99"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3 d-flex align-items-center gap-3" controlId="formAntiguedad" value={antiguedad} onChange={(e) => setAntiguedad(e.target.value)}>
+                        <Form.Label className='text-center'>Antiguedad: </Form.Label>
+                        <Form.Control type="number" min="0" max="50"/>
+                    </Form.Group>
+                    <div className='d-flex justify-content-evenly mt-4'>
+                        <Button variant='success' type="submit">Crear</Button>
+                        <Link to="/">                        
+                            <Button variant='secondary'>Cancelar</Button>
+                        </Link>
+                    </div>
+                </Form>
+            </article>
+        </section>
+    )
+}
---
+++ app/src/components/Show.jsx
@@ -0,0 +1,117 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import { db } from "../firebaseConfig/firebase";
+import { Button, Table } from "react-bootstrap";
+import Swal from "sweetalert2";
+
+export const Show = () => {
+    // Configuro los hooks
+    const [equipments, setEquipments] = useState([]);
+    // Obtengo los documentos de la db de firestore
+    const equipmentCollection = collection(db, "medicalSupplies");
+    // Función para mostrar los docs
+    const getEquipments = async () => {
+        const data = await getDocs(equipmentCollection);
+        setEquipments(
+            data.docs.map(doc => ({
+            ...doc.data(),
+                id: doc.id
+            })));
+    };
+    // Cuatro: crear función para eliminar un doc
+    const removeEquipment = async (id) => {
+        const equipmentDoc = doc(db, "medicalSupplies", id);
+        return await deleteDoc(equipmentDoc);
+    };
+    // Cinco: crear función para confirmar con Sweet Alert
+    const confirmRemove = (id) => {
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: "¡Esta acción no se puede deshacer!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Eliminar',
+            cancelButtonText: 'Cancelar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                // Llamo a la función removeEquipment
+                removeEquipment(id);
+                // Notificar que se ha eliminado
+                Swal.fire(
+                '¡Eliminado!',
+                'El equipo médico se ha eliminado.',
+                'success'
+              )
+            }
+          })
+    };
+    // Seis: utilizar useEffect
+    useEffect(() => {
+        getEquipments();
+    }, []);
+    // Siete: devolver el estado de los docs
+    if (equipments.length === 0){
+        return (
+            <div className="text-center">Loading...</div>
+        )
+    }
+    return(
+        <div className='container-fluid' id="contenedorTabla">
+            <Table bordered hover responsive id="tablaEquipamentos">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Marca</th>
+                        <th>Modelo</th>
+                        <th>Estado</th>
+                        <th>Cantidad</th>
+                        <th>Antiguedad</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {equipments.map(equipment => (
+                        <tr key={ equipment.id }>
+                            <td className="text-center">{ equipment.nombre }</td>
+                            <td className="text-center">{ equipment.marca }</td>  
+                            <td className="text-center">{ equipment.modelo }</td>
+                            <td className="text-center">{ equipment.estado }</td>
+                            <td className="text-center">{ equipment.cantidad }</td>
+                            <td className="text-center">{ `${equipment.antiguedad } año(s)`}</td>
+                            <td>
+                                <div className="d-flex justify-content-center gap-2">
+                                    <Link to={`/edit/${equipment.id}`}>
+                                        <Button variant="warning">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="-1 1 23 23" strokeWidth="2" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+                                                <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+                                                <line x1="16" y1="5" x2="19" y2="8" />
+                                            </svg>
+                                        </Button>
+                                    </Link>
+                                    <Button variant="danger" onClick={()=> confirmRemove(equipment.id)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="-1 1 23 23" strokeWidth="2" stroke="#FFFFFF" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <line x1="4" y1="7" x2="20" y2="7" />
+                                            <line x1="10" y1="11" x2="10" y2="17" />
+                                            <line x1="14" y1="11" x2="14" y2="17" />
+                                            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                        </svg>
+                                    </Button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}                    
+                </tbody>
+            </Table>
+            <Link to='/create'>
+                <Button variant="success" className="mt-2 mb-2">Agregar</Button>
+            </Link>
+        </div>
+    )
+}
