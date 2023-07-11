48ce1e17c09e7db99b5d9264f661d93d4d6d1d9c
Nicolas Diaz <52494185+Nicodc96@users.noreply.github.com>
2023-07-11 19:26:41 -0300

    (~) Se quita credenciales de firebase expuestas

--- app/src/firebaseConfig/firebase.js
+++ app/src/firebaseConfig/firebase.js
@@ -3,22 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore';
 
 // Credenciales de firebase
-/*
-    ATENCIÓN: Estas son las credenciales mías (Nicolás Díaz)
-    que usé para razones de prueba y desarrollo.
-
-    >>> allow read, write: if true;
-
-    En el futuro se deberá modificar o conservar según la
-    necesidad del grupo.
-*/
 const firebaseConfig = {
-  apiKey: "AIzaSyAbtwtQZbuifD-XQaPncyikfuiwPZf8k3A",
-  authDomain: "cac-23307-ndiaz.firebaseapp.com",
-  projectId: "cac-23307-ndiaz",
-  storageBucket: "cac-23307-ndiaz.appspot.com",
-  messagingSenderId: "1074832897697",
-  appId: "1:1074832897697:web:d691bf9e21b49989c6e2e0"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Inicializar Firebase App
